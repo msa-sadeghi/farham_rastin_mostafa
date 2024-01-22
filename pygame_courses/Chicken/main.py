2a3df4936e98a7d9251ced2f73bb68984d3b6254
msa-sadeghi <sadeghi.msai@gmail.com>
2024-01-22 16:08:57 +0330

    بازی

--- pygame_courses/Chicken/main.py
+++ pygame_courses/Chicken/main.py
@@ -1,6 +1,7 @@
 from constants import *
 from player import Player
-my_player = Player(martin_img, SCREEN_WIDTH/2,SCREEN_HEIGHT)
+
+my_player = Player(martin_img, SCREEN_WIDTH / 2, SCREEN_HEIGHT)
 running = True
 while running:
     for event in pygame.event.get():
