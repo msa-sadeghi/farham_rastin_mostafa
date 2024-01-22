78604acc4037253d0aadbe6a20044e0b6775a4cf
msa-sadeghi <sadeghi.msai@gmail.com>
2024-01-22 16:58:38 +0330

    تمرین
    تمرین: اضافه کردن متن حاوی امتیاز
    جان و شماره مرحله به بالای صفحه بازی

--- pygame_courses/Chicken/egg.py
+++ pygame_courses/Chicken/egg.py
@@ -1,2 +1,22 @@
-# TODO
-# class Egg
+from constants import *
+from pygame.sprite import Sprite
+import random
+
+
+class Egg(Sprite):
+    def __init__(self, image, x, y, type):
+        super().__init__()
+        self.image = image
+        self.rect = self.image.get_rect(topleft=(x, y))
+        self.type = type
+        self.dx = random.choice([-1, 1])
+        self.dy = random.choice([-1, 1])
+        self.speed = random.randint(1, 5)
+
+    def update(self):
+        self.rect.x += self.dx * self.speed
+        self.rect.y += self.dy * self.speed
+        if self.rect.left <= 0 or self.rect.right >= SCREEN_WIDTH:
+            self.dx *= -1
+        if self.rect.top <= 100 or self.rect.bottom >= SCREEN_HEIGHT - 100:
+            self.dy *= -1
--- pygame_courses/Chicken/main.py
+++ pygame_courses/Chicken/main.py
@@ -1,12 +1,37 @@
 from constants import *
 from player import Player
-
+from egg import Egg
+import random
+clock = pygame.time.Clock()
+FPS = 60
+level = 0
+egg_group = pygame.sprite.Group()
 my_player = Player(martin_img, SCREEN_WIDTH / 2, SCREEN_HEIGHT)
+def start_new_level():
+    global level
+    level += 1
+    for i in range(level):
+        egg1 = Egg(egg1_img, random.randint(0, SCREEN_WIDTH), random.randint(100, SCREEN_HEIGHT - 100), 0)
+        egg_group.add(egg1)
+        egg2 = Egg(egg2_img, random.randint(0, SCREEN_WIDTH), random.randint(100, SCREEN_HEIGHT - 100), 1)
+        egg_group.add(egg2)
+        egg3 = Egg(egg3_img, random.randint(0, SCREEN_WIDTH), random.randint(100, SCREEN_HEIGHT - 100), 2)
+        egg_group.add(egg3)
+        egg4 = Egg(egg4_img, random.randint(0, SCREEN_WIDTH), random.randint(100, SCREEN_HEIGHT - 100), 3)
+        egg_group.add(egg4)
+
+
+start_new_level()
+
 running = True
 while running:
     for event in pygame.event.get():
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_ESCAPE:
                 running = False
+    screen.fill((0, 0, 0))
     my_player.draw()
+    egg_group.update()
+    egg_group.draw(screen)
     pygame.display.update()
+    clock.tick(FPS)
