483c5ce1cdfaaedc80d9dd250baf2f525d7f5bbb
msa-sadeghi <sadeghi.msai@gmail.com>
2024-01-29 16:38:53 +0330

    end of egg game
    pyinstaller --onefile --windowed main.py --name="my game" --icon="icon image" --add-data="assets;assets"

--- pygame_courses/Chicken/main.py
+++ pygame_courses/Chicken/main.py
@@ -5,6 +5,11 @@ import random
 clock = pygame.time.Clock()
 FPS = 60
 level = 0
+
+target_egg_image = ""
+target_egg_type = ""
+target_egg_image_rect = ""
+
 egg_group = pygame.sprite.Group()
 my_player = Player(martin_img, SCREEN_WIDTH / 2, SCREEN_HEIGHT)
 def start_new_level():
@@ -23,6 +28,37 @@ def start_new_level():
 
 start_new_level()
 
+def draw():
+    pygame.draw.rect(screen, (245,13,210), (0,100, SCREEN_WIDTH, SCREEN_HEIGHT - 200), 4)
+    screen.blit(target_egg_image, target_egg_image_rect)
+    # TODO  display scoreboard  : score,   lives,   level number,  warps number
+
+def choose_new_target():
+    global target_egg_image, target_egg_type,target_egg_image_rect
+    target_egg = random.choice(egg_group.sprites())
+    target_egg_image = target_egg.image
+    target_egg_image_rect = target_egg_image.get_rect(bottom = 100, centerx = SCREEN_WIDTH/2)
+    target_egg_type = target_egg.type
+    
+choose_new_target()  
+
+
+def check_collisions():
+    collided_egg = pygame.sprite.spritecollideany(my_player, egg_group)
+    if collided_egg:
+        if collided_egg.type == target_egg_type:
+            collided_egg.remove(egg_group)
+            if len(egg_group) != 0:
+                choose_new_target() 
+            else:
+                start_new_level()
+                my_player.reset()
+                
+        else:
+            my_player.reset()
+            # decrease the lives
+
+
 running = True
 while running:
     for event in pygame.event.get():
@@ -30,6 +66,9 @@ while running:
             if event.key == pygame.K_ESCAPE:
                 running = False
     screen.fill((0, 0, 0))
+    check_collisions()
+    draw()
+    my_player.move()
     my_player.draw()
     egg_group.update()
     egg_group.draw(screen)
--- pygame_courses/Chicken/player.py
+++ pygame_courses/Chicken/player.py
@@ -6,6 +6,22 @@ class Player(Sprite):
         self.rect = self.image.get_rect()
         self.rect.bottom = y
         self.rect.centerx = x
+        self.velocity = 5
 
     def draw(self):
         screen.blit(self.image, self.rect)
+    
+    def reset(self):
+        self.rect.bottom = SCREEN_HEIGHT
+        self.rect.centerx = SCREEN_WIDTH/2
+        
+    def move(self):
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_UP] and self.rect.top > 100:
+            self.rect.y -= self.velocity
+        if keys[pygame.K_DOWN] and self.rect.bottom < SCREEN_HEIGHT - 100:
+            self.rect.y += self.velocity
+        if keys[pygame.K_LEFT] and self.rect.left > 0:
+            self.rect.x -= self.velocity
+        if keys[pygame.K_RIGHT] and self.rect.right < SCREEN_WIDTH:
+            self.rect.x += self.velocity
