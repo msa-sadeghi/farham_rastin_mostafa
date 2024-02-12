c5b014912f3e9274150d3e4ab1c5ec7d45b3145c
msa-sadeghi <sadeghi.msai@gmail.com>
2024-02-12 16:59:43 +0330

    a

--- pygame_courses/space/enemy.py
+++ pygame_courses/space/enemy.py
@@ -1,1 +1,15 @@
-# TODO write enemy class
+from constants import *
+from pygame.sprite import Sprite
+class Chick(Sprite):
+    def __init__(self,x,y, chick_group):
+        super().__init__()
+        self.image = pygame.image.load("assets/chick.png")
+        self.rect = self.image.get_rect(topleft=(x,y))
+        chick_group.add(self)
+        self.direction = 1
+        self.speed = 5
+        self.last_update_time = pygame.time.get_ticks()       
+    def update(self):
+        # if pygame.time.get_ticks() - self.last_update_time > 2000:
+            self.rect.x += self.direction * self.speed
+            # self.last_update_time = pygame.time.get_ticks()
--- pygame_courses/space/main.py
+++ pygame_courses/space/main.py
@@ -1,9 +1,25 @@
 from constants import *
 from player import Player
-
+from enemy import Chick
 player_bullet_group = pygame.sprite.Group()
 my_player = Player(player_bullet_group)
-
+chick_group = pygame.sprite.Group()
+def spawn_chick():
+    for i in range(4):
+        for j in range(8):
+            Chick(j * 96, i * 96, chick_group)            
+spawn_chick()
+level = 1
+def check_edge_collision():
+    on_edge = False
+    for chick in chick_group:
+        if chick.rect.left < 0 or chick.rect.right > SCREEN_WIDTH:
+            on_edge = True
+            break
+    if on_edge:
+        for chick in chick_group:
+            chick.rect.y += 10 * level
+            chick.direction *= -1
 running = True
 while running:
     for event in pygame.event.get():
@@ -12,8 +28,10 @@ while running:
                 running = False
             if event.key == pygame.K_SPACE:
                 my_player.fire()
-                
+    check_edge_collision()            
     screen.fill((0,0,0))
+    chick_group.update()
+    chick_group.draw(screen)
     player_bullet_group.update()
     player_bullet_group.draw(screen)
     my_player.draw()           
