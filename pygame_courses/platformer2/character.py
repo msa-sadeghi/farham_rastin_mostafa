73a2e760f30b71f3db6ec1b78201bd885e0bb7c3
msa-sadeghi <sadeghi.msai@gmail.com>
2024-12-30 17:01:58 +0330

    animation added

--- pygame_courses/platformer2/character.py
+++ pygame_courses/platformer2/character.py
@@ -1,6 +1,7 @@
 from pygame.sprite import Sprite
 import os
 from config import *
+import pygame.camera
 class Character(Sprite):
     def __init__(self, type, x,y, ammo, grenades):
         super().__init__()
@@ -19,14 +20,41 @@ class Character(Sprite):
                 img = pygame.image.load(f"assets/images/{self.type}/{animation}/{i}.png")
                 img_w = img.get_width()
                 img_h = img.get_height()
-                img = pygame.transform.scale(img, (img_w * 1.5, img_h * 1.5))
+                img = pygame.transform.scale(img, (img_w * 2, img_h * 2))
                 temp.append(img)
             self.all_images[animation] = temp
         self.image_number = 0
         self.action = self.animation_types[1]
         self.image = self.all_images[self.action][0]
         self.rect = self.image.get_rect(topleft=(x,y))
+        self.last_image_change_time = 0
+        self.flip = False
+        self.direction = 1
                 
     def draw(self, screen):
-        screen.blit(self.image, self.rect)        
+        img = self.all_images[self.action][self.image_number]
+        img = pygame.transform.flip(img, self.flip, False)
+        screen.blit(img, self.rect)        
+    
+    def animation(self):
+        if pygame.time.get_ticks() - self.last_image_change_time > 100:
+            self.last_image_change_time = pygame.time.get_ticks()
         
+            self.image_number += 1
+            if self.image_number >= len(self.all_images[self.action]):
+                self.image_number = 0
+    def move(self, moving_left, moving_right):
+        dx = 0
+        if moving_left:
+            dx -= 5
+            self.direction = -1
+            self.flip = True
+        if moving_right:
+            dx += 5
+            self.direction = 1
+            self.flip = False
+            
+        self.rect.x += dx
+    def change_action(self,new_animation):
+        pass
+                
--- pygame_courses/platformer2/main.py
+++ pygame_courses/platformer2/main.py
@@ -1,14 +1,27 @@
 from config import *
 from character import Character
 clock = pygame.time.Clock()
-
+pygame.init()
 player = Character("player", 100, 300, 60, 10)
-
+moving_left, moving_right = (False, False)
 running = True
 while running == True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-    player.draw(screen)       
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                moving_left = True
+            if event.key == pygame.K_RIGHT:
+                moving_right = True
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_LEFT:
+                moving_left = False
+            if event.key == pygame.K_RIGHT:
+                moving_right = False
+    screen.fill((0,0,0))       
+    player.draw(screen)  
+    player.move(moving_left, moving_right)
+    player.animation()     
     pygame.display.update()
     clock.tick(FPS)
